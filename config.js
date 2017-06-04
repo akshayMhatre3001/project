04b87a12f98d9bf7bb987821e60c456884085f32
beastf589 <beastf589@hotmail.com>
2017-06-04 11:05:04 +0530

    config

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'secret': 'MYSECRETKEY007',
+    'database':'mongodb://localhost:27017/elearnfinal89',
+    //'database':'mongodb://hemant1:Hemant1@ds157469.mlab.com:57469/elearndb',
+    //'PORT1':process.env.PORT || 3001
+     'PORT':process.env.PORT || 3001
+};
